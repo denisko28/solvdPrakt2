c74eeb8a4b6f1c3281d09b29ec701e52d1397346
Денис Харитеску <deniskharitesku@MacBook-Air-Denis-2086.local>
2022-04-17 18:19:15 +0300

    Updated 'pay' method inside of the interface

--- src/main/java/com/solvd/bank/Interfaces/Payable.java
+++ src/main/java/com/solvd/bank/Interfaces/Payable.java
@@ -1,8 +1,7 @@
 package com.solvd.bank.Interfaces;
 
 import com.solvd.bank.Classes.Account;
-import com.solvd.bank.Classes.Transaction;
 
 public interface Payable {
-    public Transaction pay(Account from, Account to, float amount);
+    public void pay(Account from, Account to, float amount, IPayCallBack callBackFunc);
 }
